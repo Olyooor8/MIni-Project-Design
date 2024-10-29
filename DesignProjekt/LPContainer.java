fb9ace47702b9ff4fb26f54e12ea712bf8070c5e
Niclas1806 <10628062@ucn.dk>
2024-10-29 11:44:56 +0100

    Create LPContainer.java

---
+++ DesignProjekt/LPContainer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class LPContainer
+{
+    private static LPContainer uniqueInstance;
+    private ArrayList<LP>ListLPs;
+    
+    private LPContainer(){
+        ListLPs = new ArrayList<>();
+    }
+    
+    public void addLP(LP l){
+        ListLPs.add(l);
+    }
+    
+    public static LPContainer getUniqueInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new LPContainer();
+        }
+        return uniqueInstance;
+    }
+}
