80408dacd7da62f2598926d86d9a7a874b7aa3f2
Olyooor8 <10632130@ucn.dk>
2024-10-29 11:55:15 +0100

    123

---
+++ DesignProjekt/CopyController.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class CopyController here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CopyController
+{
+    private Copy selectedCopy;
+    
+    
+    public Copy findCopyBySerial(String serialNumber){
+        
+        
+        return null;
+    }
+}
