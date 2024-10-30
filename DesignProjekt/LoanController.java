0052eace31b0e4c3c8773c4adf0da5c41c92e596
Lasse Poulsen <10632094@ucn.dk>
2024-10-30 11:37:51 +0100

    Controller comments

--- DesignProjekt/LoanController.java
+++ DesignProjekt/LoanController.java
@@ -1,9 +1,8 @@
 
 /**
- * Write a description of class LoanController here.
+ * The main controller of the system, this class contains the systems main methods, creating and building the loans.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Gruppe5
  */
 public class LoanController
 {
@@ -15,7 +14,8 @@ public class LoanController
 
     /**
      * Constructor for objects of class LoanController
-     * @param copy The CopyController
+     * @param copy The CopyController that becomes assigned to the Loan Controller
+     * @param lender The LenderController that becomes assigned to the Loan Controller
      */
     public LoanController(CopyController copy, LenderController lender)
     {
@@ -23,30 +23,50 @@ public class LoanController
         assignedLenderController = lender;
         loanContainer = LoanContainer.getUniqueInstance();
     }
-
+    
+    /**
+     * Create an instance of Loan and assign it to the Loan Controller.
+     * @param loanDuration The duration of the loan in days
+     * @param loanNumber The identifying number for the loan
+     */
     public void startLoan(int loanDuration, String loanNumber)
     {
         currentLoan = new Loan(loanDuration, loanNumber);
     }
     
+    /**
+     * Find and assign a lender to the current Loan instance, by searching through names
+     * @param name The name that the system should try and find
+     */
     public Lender findLenderName(String name){
         Lender foundLender = assignedLenderController.findLenderByName(name);
         currentLoan.setLender(foundLender);
         return foundLender;
     }
     
+    /**
+     * Find and assign a lender to the current Loan instance, by searching through numbers
+     * @param number The number that the system should try and find
+     */
     public Lender findLenderNumber(String number){
         Lender foundLender = assignedLenderController.findLenderByNumber(number);
         currentLoan.setLender(foundLender);
         return foundLender;
     }
     
+    /**
+     * Find and assign a copy to the current Loan instance, by searching through serial numbers
+     * @param serialNumber The serial number that the system should try and find
+     */
     public Copy findCopy(String serialNumber){
         Copy foundCopy = assignedCopyController.findCopyBySerial(serialNumber);
         currentLoan.setCopy(foundCopy);
         return foundCopy;
     }
     
+    /**
+     * Confirm and finalize the loan, adding it to the Loan Container list
+     */
     public void confirm(){
         loanContainer.addLoan(currentLoan);
         currentLoan = null;
